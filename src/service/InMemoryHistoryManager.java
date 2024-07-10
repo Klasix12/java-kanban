70fca432fdf6f0ff5fa3f151b83dc3f371d413c2
Artem Oleshev <sunlightpattern@yandex.ru>
2024-07-11 02:18:24 +0300

    add history size constant

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -6,14 +6,23 @@ import java.util.ArrayList;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final ArrayList<Task> history = new ArrayList<>();
+    private static final int HISTORY_SIZE = 10;
 
     @Override
     public void add(Task task) {
+        if (history.size() == HISTORY_SIZE) {
+            history.removeFirst();
+        }
         history.add(task);
     }
 
     @Override
     public ArrayList<Task> getHistory() {
         return new ArrayList<>(history);
+    }
+
+    @Override
+    public int getHistorySize() {
+        return HISTORY_SIZE;
     }
 }
