a45b09e815e24dc377fe23051ba4d534b1e7e275
Artem Oleshev <sunlightpattern@yandex.ru>
2024-07-10 20:08:34 +0300

    remove equals override

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -28,16 +28,4 @@ public class Subtask extends Task {
     public int hashCode() {
         return Objects.hash(name, description, id, status, epicId);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || obj.getClass() != getClass()) return false;
-        Subtask otherSubtask = (Subtask) obj;
-        return Objects.equals(name, otherSubtask.name) &&
-                Objects.equals(description, otherSubtask.description) &&
-                Objects.equals(id, otherSubtask.id) &&
-                Objects.equals(status, otherSubtask.status) &&
-                Objects.equals(epicId, otherSubtask.epicId);
-    }
 }
