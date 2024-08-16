815f631a1e5930e6751ad4e26a508a0a5c3e0fcd
Artem Oleshev <sunlightpattern@yandex.ru>
2024-08-16 12:29:35 +0300

    remove history size

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -8,5 +8,4 @@ public interface HistoryManager {
 
     void add(Task task);
     ArrayList<Task> getHistory();
-    int getHistorySize();
 }
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -7,25 +7,14 @@ import java.util.LinkedList;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final LinkedList<Task> history = new LinkedList<>();
-    private static final int HISTORY_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            if (history.size() == HISTORY_SIZE) {
-                history.removeFirst();
-            }
-            history.add(task);
-        }
+        history.add(task);
     }
 
     @Override
     public ArrayList<Task> getHistory() {
         return new ArrayList<>(history);
-    }
-
-    @Override
-    public int getHistorySize() {
-        return HISTORY_SIZE;
     }
 }
