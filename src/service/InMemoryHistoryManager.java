8a8f56d72f3a0408fd9ea65299f749815ef54c13
Artem Oleshev <sunlightpattern@yandex.ru>
2024-08-17 15:48:40 +0300

    add null check

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -78,7 +78,9 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void remove(int id) {
         Node node = history.get(id);
-        removeNode(node);
-        history.remove(id);
+        if (node != null) {
+            removeNode(node);
+            history.remove(id);
+        }
     }
 }
