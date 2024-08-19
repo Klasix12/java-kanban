fcf46334681268ca941cde30f020341d0508dd48
Artem Oleshev <sunlightpattern@yandex.ru>
2024-08-19 11:47:29 +0300

    add new tests

--- test/service/HistoryManagerTest.java
+++ test/service/HistoryManagerTest.java
@@ -46,6 +46,109 @@ public class HistoryManagerTest {
     }
 
     @Test
+    public void testHistoryManagerIsEmptyWhenTasksDeleted() {
+        final int taskId = taskManager.addTask(task);
+        Task task2 = new Task("task2", "");
+        final int task2Id = taskManager.addTask(task2);
+        taskManager.addTask(task);
+        taskManager.addTask(task2);
+        taskManager.getTaskById(task2Id);
+        taskManager.getTaskById(taskId);
+        taskManager.clearTasks();
+        Assertions.assertTrue(taskManager.getHistory().isEmpty());
+    }
+
+    @Test
+    public void testHistoryManagerListIsEmptyWhenEpicsDeleted() {
+        Epic epic = new Epic("Epic", "");
+        Epic epic2 = new Epic("Epic2", "");
+
+        final int epicId = taskManager.addEpic(epic);
+        final int epic2Id = taskManager.addEpic(epic2);
+
+        Subtask subtask = new Subtask("Subtask", "", epicId);
+        Subtask subtask2 = new Subtask("Subtask2", "", epicId);
+        Subtask subtask3 = new Subtask("Subtask3", "", epic2Id);
+        Subtask subtask4 = new Subtask("Subtask4", "", epic2Id);
+
+        final int subtaskId = taskManager.addSubtask(subtask);
+        final int subtask2Id = taskManager.addSubtask(subtask2);
+        final int subtask3Id = taskManager.addSubtask(subtask3);
+        final int subtask4Id = taskManager.addSubtask(subtask4);
+
+        taskManager.getEpicById(epicId);
+        taskManager.getEpicById(epic2Id);
+
+        taskManager.getSubtaskById(subtaskId);
+        taskManager.getSubtaskById(subtask2Id);
+        taskManager.getSubtaskById(subtask3Id);
+        taskManager.getSubtaskById(subtask4Id);
+
+        taskManager.clearEpics();
+        Assertions.assertTrue(taskManager.getHistory().isEmpty());
+    }
+
+    @Test
+    public void testHistoryManagerListHasCorrectSizeWhenSubtasksDeleted() {
+        Epic epic = new Epic("Epic", "");
+        Epic epic2 = new Epic("Epic2", "");
+
+        final int epicId = taskManager.addEpic(epic);
+        final int epic2Id = taskManager.addEpic(epic2);
+
+        Subtask subtask = new Subtask("Subtask", "", epicId);
+        Subtask subtask2 = new Subtask("Subtask2", "", epicId);
+        Subtask subtask3 = new Subtask("Subtask3", "", epic2Id);
+        Subtask subtask4 = new Subtask("Subtask4", "", epic2Id);
+
+        final int subtaskId = taskManager.addSubtask(subtask);
+        final int subtask2Id = taskManager.addSubtask(subtask2);
+        final int subtask3Id = taskManager.addSubtask(subtask3);
+        final int subtask4Id = taskManager.addSubtask(subtask4);
+
+        taskManager.getEpicById(epicId);
+        taskManager.getEpicById(epic2Id);
+
+        taskManager.getSubtaskById(subtaskId);
+        taskManager.getSubtaskById(subtask2Id);
+        taskManager.getSubtaskById(subtask3Id);
+        taskManager.getSubtaskById(subtask4Id);
+
+        taskManager.clearSubtasks();
+        Assertions.assertEquals(2, taskManager.getHistory().size());
+    }
+
+    @Test
+    public void testHistoryManagerListHasCorrectSizeWhenEpicDeleted() {
+        Epic epic = new Epic("Epic", "");
+        Epic epic2 = new Epic("Epic2", "");
+
+        final int epicId = taskManager.addEpic(epic);
+        final int epic2Id = taskManager.addEpic(epic2);
+
+        Subtask subtask = new Subtask("Subtask", "", epicId);
+        Subtask subtask2 = new Subtask("Subtask2", "", epicId);
+        Subtask subtask3 = new Subtask("Subtask3", "", epic2Id);
+        Subtask subtask4 = new Subtask("Subtask4", "", epic2Id);
+
+        final int subtaskId = taskManager.addSubtask(subtask);
+        final int subtask2Id = taskManager.addSubtask(subtask2);
+        final int subtask3Id = taskManager.addSubtask(subtask3);
+        final int subtask4Id = taskManager.addSubtask(subtask4);
+
+        taskManager.getEpicById(epicId);
+        taskManager.getEpicById(epic2Id);
+
+        taskManager.getSubtaskById(subtaskId);
+        taskManager.getSubtaskById(subtask2Id);
+        taskManager.getSubtaskById(subtask3Id);
+        taskManager.getSubtaskById(subtask4Id);
+
+        taskManager.removeEpicById(epicId);
+        Assertions.assertEquals(3, taskManager.getHistory().size());
+    }
+
+    @Test
     public void testHistoryManagerCorrectTaskSequence() {
         final int taskId = taskManager.addTask(task);
 
