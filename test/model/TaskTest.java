47db016c8d477921533c49e6b2e54211d7bd29b6
Artem Oleshev <sunlightpattern@yandex.ru>
2024-08-20 22:48:09 +0300

    fix imports

--- test/model/TaskTest.java
+++ test/model/TaskTest.java
@@ -1,8 +1,8 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskTest {
     @Test
