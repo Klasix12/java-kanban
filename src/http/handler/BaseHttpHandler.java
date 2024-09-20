8c8fb1c5cb0e2c4367d1860213c4a271ecce40dd
Artem Oleshev <sunlightpattern@yandex.ru>
2024-09-20 17:55:46 +0300

    add try with catch resource

--- src/http/handler/BaseHttpHandler.java
+++ src/http/handler/BaseHttpHandler.java
@@ -2,15 +2,13 @@ package http.handler;
 
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import http.RequestMethod;
-import serialization.LocalDateTimeAdapter;
 import service.TaskManager;
+import util.Managers;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 
 public class BaseHttpHandler {
     protected final TaskManager taskManager;
@@ -18,57 +16,73 @@ public class BaseHttpHandler {
 
     public BaseHttpHandler(TaskManager taskManager) {
         this.taskManager = taskManager;
-        gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter()).create();
+        gson = Managers.getDefaultGson();
     }
 
-    protected void sendText(HttpExchange exchange, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
-        exchange.sendResponseHeaders(200, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendText(HttpExchange exchange, String text) {
+        try (exchange) {
+            byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+            exchange.sendResponseHeaders(200, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
-    protected void sendTaskCreated(HttpExchange exchange, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
-        exchange.sendResponseHeaders(201, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendTaskCreated(HttpExchange exchange, String text) {
+        try (exchange) {
+            byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            exchange.sendResponseHeaders(201, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
-    protected void sendIncorrectUserData(HttpExchange exchange, String text) throws IOException {
-        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
-        exchange.sendResponseHeaders(400, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendIncorrectUserData(HttpExchange exchange, String text) {
+        try (exchange) {
+            byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            exchange.sendResponseHeaders(400, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
-    protected void sendNotFound(HttpExchange exchange) throws IOException {
-        byte[] resp = "Not Found".getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
-        exchange.sendResponseHeaders(404, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendNotFound(HttpExchange exchange) {
+        try (exchange) {
+            byte[] resp = "Not Found".getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            exchange.sendResponseHeaders(404, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
-    protected void sendMethodNotAllowed(HttpExchange exchange) throws IOException {
-        byte[] resp = "Method Not Allowed".getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
-        exchange.sendResponseHeaders(405, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendMethodNotAllowed(HttpExchange exchange) {
+        try (exchange) {
+            byte[] resp = "Method Not Allowed".getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            exchange.sendResponseHeaders(405, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
-    protected void sendHasInteractions(HttpExchange exchange) throws IOException {
-        byte[] resp = "Not Acceptable".getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
-        exchange.sendResponseHeaders(406, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
+    protected void sendHasInteractions(HttpExchange exchange) {
+        try (exchange) {
+            byte[] resp = "Not Acceptable".getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "text/html;charset=utf-8");
+            exchange.sendResponseHeaders(406, resp.length);
+            exchange.getResponseBody().write(resp);
+        } catch (IOException e) {
+            throw new RuntimeException("Ошибка при отправке ответа: " + e.getMessage());
+        }
     }
 
     protected RequestMethod extractRequestMethod(HttpExchange exchange) {
